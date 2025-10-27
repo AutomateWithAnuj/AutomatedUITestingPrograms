92263065d134b27ffa05820f7bebd4858726005e
Anuj Rajput <anujrajput8959@gmail.com>
2025-10-27 14:44:38 +0530

    27-10-2025 Update

--- src/test/java/com/AutomateWithAnuj/_26_JavaScriptExecutor/TestSelenium49_JSEx_NavigateToURL_Scroll.java
+++ src/test/java/com/AutomateWithAnuj/_26_JavaScriptExecutor/TestSelenium49_JSEx_NavigateToURL_Scroll.java
@@ -1,17 +1,15 @@
 package com.AutomateWithAnuj._26_JavaScriptExecutor;
 
 import com.AutomateWithAnuj._07_CommonToAllCode.TestSelenium09_CommonToAll;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class TestSelenium49_NavigateToURL extends TestSelenium09_CommonToAll {
+public class TestSelenium49_JSEx_NavigateToURL_Scroll extends TestSelenium09_CommonToAll {
     @Test
     public void TestSelenium49_NavigateToURL(){
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.location = 'https://selectorshub.com/xpath-practice-page/'");
-        
+
         js.executeScript("window.scrollBy(0,500)");
 
     }
